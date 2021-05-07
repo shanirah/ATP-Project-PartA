10bc596a0f21eb3d000f9e6dde4ca228dd5f1c76
shanrahi <shanirah@post.bgu.ac.ul>
2021-05-08 00:28:04 +0300

    MyMaze compressor and Decompressor added added toByteArray function and new maze constructor

--- src/test/RunCompressDecompressMaze.java
+++ src/test/RunCompressDecompressMaze.java
@@ -14,7 +14,7 @@ public class RunCompressDecompressMaze {
     public static void main(String[] args) throws Exception {
         String mazeFileName = "savedMaze.maze";
         AMazeGenerator mazeGenerator = new MyMazeGenerator();
-        Maze maze = mazeGenerator.generate(1000, 1000); //Generate new maze
+        Maze maze = mazeGenerator.generate(100, 100); //Generate new maze
         try {
             // save maze to a file
             OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileName));
